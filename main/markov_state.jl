6ac27d7757df13a66e5e4ef7082d14361e2d5117
Yue <yueshang@sas.upenn.edu>
2026-01-03 15:14:56 -0500

    initialize the main program to do MSM analysis

--- main/markov_state.jl
+++ main/markov_state.jl
@@ -7,53 +7,19 @@ network_id=parse(Int, ARGS[2])
 taskid=parse(Int, ARGS[3])
 trainT=parse(Float64, ARGS[4])
 seed=parse(Int, ARGS[5])
+
 testT=parse(Float64, ARGS[6])
+strain_source=parse(Float64, ARGS[7])
+seed2=parse(Int, ARGS[8])
 
-
-function simulate_and_record(
-    enm::ENM;
-    steps::Int,
-    save_every::Int,
-    Φ::Matrix{Float64},
-    out_edge::Int,
-    advance!::Function,
-    savepath::Union{Nothing,String}=nothing,
- )
-    k = size(Φ, 2)
-    nframes = Int(floor(steps / save_every)) + 1
-
-    A    = Matrix{Float64}(undef, nframes, k)
-    sout = Vector{Float64}(undef, nframes)
-    t    = Vector{Int}(undef, nframes)
-    frame = 1
-    A[frame, :] .= project_modes_rigid(enm, Φ)   # <<< UPDATED LINE
-    sout[frame] = cal_strain(enm, out_edge)
-    t[frame] = 0
-
-    # ---- MD loop ----
-    for s in 1:steps
-        advance!(enm)
-
-        if s % save_every == 0
-            frame += 1
-            A[frame, :] .= project_modes_rigid(enm, Φ)  # <<< UPDATED LINE
-            sout[frame] = cal_strain(enm, out_edge)
-            t[frame] = s
-        end
-    end
-
-    if savepath !== nothing
-        @save savepath A sout t
-    end
-
-    return (A=A, sout=sout, t=t)
-end
 
 traintime=10_000
 alpha=50.0
 timewindow=200
 
-println("Dimension: $(dim), Network ID: $(network_id), taskid: $(taskid) trainT : $(trainT) testT: $(testT)")
+record_per = 5000 #50tau
+n_frames= 2000
+
 
 root="/data2/shared/yueshang/julia/"
 net_file =  "/data2/shared/yueshang/julia/dim$(dim)/network$(network_id)/network.txt"
@@ -63,11 +29,33 @@ enm=ENM(net_file)
 input,output=load_task(task_path)
 
 load_k(enm, joinpath(task_path, "trainT$(trainT)_alpha$(alpha)_tw$(timewindow)/seed$(seed)/k$(traintime).f64"))
-dim=enm.dim
-if dim==2
-    m0_num=3
-elseif dim==3
-    m0_num=6
+n_soft=10
+phi,lambda = mode_basis(enm, k=n_soft)
 
-Φ, λ = compute_soft_modes(enm, 10+m0_num)
+#put strain
+put_strain!(enm, input[1][1], strain_source)
 
+test_path=joinpath(task_path, "MSM_testT$(testT)_strain$(strain_source)_seed$(seed)/")
+mkpath(test_path)
+data    = Matrix{Float32}(undef, n_frames, n_soft)
+sout= Vector{Float32}(undef, n_frames)
+for stepid in 1:n_frames
+    
+    rng=StableRNG(seed2+stepid)
+    run_md!(enm,testT,steps=record_per, rng=rng)
+    data[stepid, :] .= project_modes_rigid(enm, phi)
+    sout[stepid] = cal_strain(enm, output[1][1])
+
+end
+
+open(joinpath(test_path, "project10modes.f32"), "w") do file
+    write(file, Float32.(data))
+end
+
+open(joinpath(test_path, "strain.f32"), "w") do file
+    write(file, Float32.(sout))
+end
+
+TICA_=tica(data;lag=10,k=3)
+
+open(join)
